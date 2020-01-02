b3480e7504c6b7d998820bc69878d48db6c87b67
George Izot <urka94@bk.ru>
2020-01-02 21:42:07 +0300

    Add padding for Dropdown, finished Date-mask

---
+++ src/components/date-mask/date-mask.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+$('input[type=text] .date-mask').mask('ДД.ММ.ГГГГ');
+ });
--- src/pages/ui-kit/ui-kit.js
+++ src/pages/ui-kit/ui-kit.js
@@ -1,5 +1,7 @@
 
 import '../../components/dropdown/dropdown'
+import '../../components/date-mask/date-mask'
+import '../../js/jquery.mask.min.js'
 //SCSS imports
 import './ui-kit.scss'
 
